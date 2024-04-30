4ab1255370f5bd2e96b7cd276b418a658c7fa4b2
Alejandra_Machuca <am3793825@gmail.com>
2024-04-30 17:33:40 -0500

    holi

--- Proyecto.py
+++ Proyecto.py
@@ -12,25 +12,59 @@ print("---------------------------")
 print("----------MENÚ-------------")
 print("---------------------------")
 print("")
-print("Bienvenidos, escoge una de las opciones de nuestro menú: \n1. Campers\n2. trainers\n3. coordinador")
+print("Bienvenidos, escoge una de las opciones de nuestro menú: \n1. Campers\n2. Trainers\n3. Coordinador")
 x = int(input("Elige la opción que deseas: "))
 información=[]
 if(x==1):
    info=abrirArchivo()
    for i in info[0]["alumnos"]:
-       print("ingrese la contraseña del camper")
+       print("ingrese la contraseña: ")
        print(input(""))
+       idAlumno=input("Ingrese el ID del camper")
+       idAlumno=int(input(""))
+
+       def main():
+          campers=input("Ingrese su nombre de usuario: ")
+          contraseña=input("Ingrese su contraseña: ")
+          verificarContraseña(campers, contraseña)
        print("----------------------")
        print("ID:", i["id"])
        print("Nombre:", i["nombre"])
        print("Apellido:", i["apellido"])
        print("Acudiente:", i["acudiente"])
-       print("direccion:", i["direccion"])
+       print("Direccion:", i["direccion"])
        print("Numero de celular:", i["numero_celular"])
        print("Numero fijo", i["numero_fijo"])
        print("Estado:", i["estado"])
        print("Riesgo:", i["riesgo"])
        print("----------------------")
        print("")
+       def contraseñasCampers():
+          with open("contraseñas.json","r")as file:
+             return json.load(file)
+       def verificarContraseña(campers, contraseña):
+          contraseñas=contraseñasCampers()
+          if campers in contraseñas["campers"]and contraseñas["campers"][campers]==contraseñas:
+             print("Contraseña correcta")
+          else:
+             print("Contraseña incorrecta")
+       def main():
+          campers=input("Ingrese su nombre de usuario: ")
+          contraseña=input("Ingrese su contraseña: ")
+          verificarContraseña(campers, contraseña)
+if(x==2):
+   info=abrirArchivo
+   for i in ["2"]:
+    print("------------------------")
+    print("Trainer:", i["trainer"])
+    print("Salón", i["salon"])
+    print("Ruta", i["ruta"])
+    print("Módulo", i["modulo"])
+    print("Hora", i["hora"])
+if(x==3):
+   info=abrirArchivo, {
+      "coordinador"
+   }
+   print("tú puedes:)")
 
 
